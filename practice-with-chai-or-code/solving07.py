02e6838448215a2c2e86e4e1037332c604146fa5
am-sazid <amsazid.cse@gmail.com>
2025-01-21 22:12:49 +0600

    problem1-7solve

---
+++ practice-with-chai-or-code/solving07.py
@@ -0,0 +1,24 @@
+
+# numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+
+# for i in numbers:
+#     if i % 2 == 0:
+#         print(i)
+        
+# numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+
+# positive_number_count = 0
+
+# for num in numbers:
+#     if num > 0:
+        # positive_number_count += 1
+# print(positive_number_count)
+
+# numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+
+# total_numbers = 0
+# for num in numbers:
+#     if num > 0 :
+#         print(num)
+#         total_numbers += 1
+# print("Total Positive Number :",total_numbers)
---
+++ practice-with-chai-or-code/solving08.py
@@ -0,0 +1,15 @@
+
+n = 24
+sum = 0
+for i in range(1,n+1):
+    if i % 2 == 0 :
+        sum += i
+print(sum)
+
+
+# n = 10
+# sum = 0
+# for i in range(1,n+1):
+#     if i % 2 == 0 :
+#         sum += 1
+# print(sum)       
