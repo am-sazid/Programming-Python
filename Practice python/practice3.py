1695edbd59cce04466acc680aff6b6d43c307c52
am-sazid <amsazid.cse@gmail.com>
2025-02-22 00:00:31 +0600

    solving1

--- Practice python/practice3.py
+++ Practice python/practice3.py
@@ -1,15 +1,21 @@
 
-class person:
-    name = "anonymous"
+# class person:
+#     name = "anonymous"
     
-    def change_name(self,name):
-        self.name = name
-    @classmethod
-    def change_name(self,name):
-        self.name = name
+#     def change_name(self,name):
+#         self.name = name
+#     @classmethod
+#     def change_name(self,name):
+#         self.name = name
     
         
-p1 = person()
-p1.change_name("Sazid")
-# print(p1.name)
-print(person.name)
+# p1 = person()
+# p1.change_name("Sazid")
+# # print(p1.name)
+# print(person.name)
+
+X,Y = map(int,input().split())
+
+sum = X + Y *10
+
+print(sum)
---
+++ Practice python/solving1.py
@@ -0,0 +1,20 @@
+
+
+# int_value = [1,2,3,5,865,343,86834,234556,765]
+
+# max_value = int_value[0]
+
+# for value in int_value:
+#     if value < max_value:
+#         max_value = value
+# print(max_value)
+
+hour = [0,1,2,3,4,5]
+terget = 2
+
+num_epm = 0
+
+for i in hour:
+    if i >= terget :
+        num_epm += 1
+print(num_epm)
