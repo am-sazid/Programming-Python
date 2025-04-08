b0d905465a17d503d9328e2315f37df8438cb84f
am-sazid <amsazid.cse@gmail.com>
2025-04-08 11:20:07 +0600

    codecheefproblem1

---
+++ problem-solving/codecheef1.py
@@ -0,0 +1,18 @@
+
+t = int(input())
+for i in range(t):
+    N = int(input())
+
+    if N % 3 == 0:
+        print("YES")
+    else:
+        print("NO")
+    
+
+    if N % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+
+
