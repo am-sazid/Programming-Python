2b90a52a27851d67abf99113b8f2411809f44359
am-sazid <amsazid.cse@gmail.com>
2025-04-08 10:55:10 +0600

    solving2

---
+++ practice-with-chai-or-code/loop/problem2.py
@@ -0,0 +1,11 @@
+
+items = ["apple", "banana","banana", "orange", "mango"]
+
+uniqe_items = set()
+
+for item in items:
+    if item in uniqe_items:
+        print("Duplicate item found:", item)
+        break
+    uniqe_items.add(item)
+
