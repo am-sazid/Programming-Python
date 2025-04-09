520d7658a68c04ce7978195ac3a1c24a24187763
am-sazid <amsazid.cse@gmail.com>
2025-04-10 00:33:00 +0600

    Solving

--- practice-with-chai-or-code/loop/problem1.py
+++ practice-with-chai-or-code/loop/problem1.py
@@ -1,8 +1,32 @@
 
-while True:
-    user = int(input("Enter a number 1 -10 :"))
-    if 1 <= user <= 10:
-        print("Your Number: ",user)
+# while True:
+#     user = int(input("Enter a number 1 -10 :"))
+#     if 1 <= user <= 10:
+#         print("Your Number: ",user)
+#         break
+#     print("Wrong input Try again!")
+# N = int(input())
+
+# # Read the list of numbers
+# numbers = list(map(int, input().split()))
+
+# # Check if the list is in ascending order
+
+# if numbers == sorted(numbers):
+#     print("Yes")
+# else:
+#     print("No")
+
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
+for i in range(N):
+    if A == sorted(A) and N == len(A):
+        print("YES")
         break
-    print("Wrong input Try again!")
+    else:
+        print("NO")
+        break
 
