1c32c40ae6db720bc8d933eb7c2db07e3e250b50
am-sazid <amsazid.cse@gmail.com>
2025-04-11 01:28:15 +0600

    solving3

--- practice-with-chai-or-code/loop/problem2.py
+++ practice-with-chai-or-code/loop/problem2.py
@@ -1,13 +1,21 @@
 
-items = ["apple", "banana","banana", "orange", "mango"]
+# items = ["apple", "banana","banana", "orange", "mango"]
 
-uniqe_items = set()
+# uniqe_items = set()
 
-for item in items:
-    if item in uniqe_items:
-        print("Duplicate item found:", item)
-        break
-    uniqe_items.add(item)
+# for item in items:
+#     if item in uniqe_items:
+#         print("Duplicate item found:", item)
+#         break
+#     uniqe_items.add(item)
 
 
+X = int(input())
+Y = int(input())
 
+if X < Y :
+    print("YES")
+else:
+    print("NO")
+
+# print("We need to understand", "how to print multiple output in a single line")
