adf71cf712d70f272f98a5742b11942d600fab65
am-sazid <amsazid.cse@gmail.com>
2025-04-13 22:21:26 +0600

    codechef4

---
+++ problem-solving/codechef.py
@@ -0,0 +1,24 @@
+
+t = int(input())
+
+for i in range(t):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    if len(arr) == N:
+        print(max(arr))
+
+
+# t = int(input())
+
+# for _ in range(t):
+#     n = int(input())
+    
+#     arr = [int(x) for x in input().split()]
+    
+#     maximum = arr[0]
+#     for i in range(1, n):
+#         if arr[i] > maximum:
+#             maximum = arr[i]
+    
+#     print(maximum)
+
---
+++ problem-solving/codechef3.py
@@ -0,0 +1,10 @@
+N , X = map(int, input().split())
+A = list(map(int, input().split()))
+
+
+
+if X in A and len(A) ==N:
+    print("YES")
+
+else:
+    print("NO")
