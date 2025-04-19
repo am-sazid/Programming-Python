6dc88b8ec70b7c59a26447f9b3b878b0ac1d4994
am-sazid <amsazid.cse@gmail.com>
2025-04-19 08:24:16 +0600

    01-solving

---
+++ practice-with-chai-or-code/problem-in-fuction/problem1.py
@@ -0,0 +1,15 @@
+
+def square(n):
+    return n **2
+
+print(square(5))
+
+def add(n,m):
+    return n + m
+
+print(add(5,6))
+
+def multiply(n,m):
+    return n * m
+print(multiply('a',6))
+print(multiply(5,6))
