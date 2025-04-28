698364aec98a3f6160c038e8a238cf4aa0e97385
am-sazid <amsazid.cse@gmail.com>
2025-04-28 23:25:59 +0600

    Test

---
+++ problem-solving/solving03.py
@@ -0,0 +1,16 @@
+
+marks = [67,89,36,78,90,100,53,45]
+
+failmarks = False
+
+for mark in marks:
+    if mark < 33:
+        failmarks = True
+       
+        break
+        
+if failmarks:
+    print("You have failed the exam")
+else:
+    print(sum(marks))
+        
