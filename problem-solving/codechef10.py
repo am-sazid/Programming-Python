d428d4c8bec73f6f338af7ed017b932612faf6e0
am-sazid <amsazid.cse@gmail.com>
2025-05-28 22:28:07 +0600

    CodeChef 10

---
+++ problem-solving/codechef10.py
@@ -0,0 +1,8 @@
+# cook your dish here
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+    odd_days_total = sum(A[i] for i in range(0, N, 2))  # Days 1,3,5,...
+    even_days_total = sum(A[i] for i in range(1, N, 2)) # Days 2,4,6,...
+    print(max(odd_days_total, even_days_total))
