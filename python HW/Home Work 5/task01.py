15c2876208e911777b5f7048a75214077b0cbae4
am-sazid <amsazid.cse@gmail.com>
2025-10-29 23:17:23 +0600

    Python HW 5

---
+++ python HW/Home Work 5/task01.py
@@ -0,0 +1,10 @@
+num = 5
+
+if num >= 0:
+    print(num," is a positive number.")
+print("This is always printed.")
+
+num = -5
+if num > 0 :
+    print(num," is a positive number.")
+print("This is also always printed.")
